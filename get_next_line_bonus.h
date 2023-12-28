8b4e0635f32dee4641f9c65e2e8731006810be6f
Ivan Romero Soto <ivromero@student.42urduliz.com>
2023-12-28 20:15:51 +0100

    Funcionando con el bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivromero <ivromero@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/16 00:21:10 by ivromero          #+#    #+#             */
+/*   Updated: 2023/12/28 20:01:51 by ivromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t count, size_t size);
+void	*ft_memset(void *b, int c, size_t len);
+void	*ft_memmove(void *dst, const void *src, size_t len);
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,85 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ivromero <ivromero@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/16 00:20:50 by ivromero          #+#    #+#             */
+/*   Updated: 2023/12/28 20:01:41 by ivromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	while (len--)
+		((char *)b)[len] = (unsigned char)c;
+	return (b);
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*pntrs;
+
+	pntrs = malloc(size * count);
+	if (pntrs)
+		ft_memset(pntrs, 0, size * count);
+	return (pntrs);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = -1;
+	while (s[++i])
+		;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != '\0' || (c % 256) == '\0')
+		if (*s++ == (c % 256))
+			return ((char *)--s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	char	*pstr;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	pstr = str;
+	while (*s1)
+		*(str++) = *(s1++);
+	while (*s2)
+		*(str++) = *(s2++);
+	*str = '\0';
+	return (pstr);
+}
+
+/*
+char	*ft_strdup(const char *s)
+{
+	size_t	i;
+	size_t	len;
+	char	*dest;
+
+	dest = 0;
+	i = -1;
+	len = ft_strlen(s);
+	dest = (char *)ft_calloc(len + 1, sizeof(*dest));
+	if (dest)
+		while (++i < len)
+			dest[i] = s[i];
+	return (dest);
+}
+*/
